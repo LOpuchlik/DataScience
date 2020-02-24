289b63e7d043210eb0753b6860e1b3df155ccd50
LOpuchlik <lidia.opuchlik@gmail.com>
2020-02-24 22:23:55 +0100

    Added preprocessing of dataset, model of multiple variable regression, feature choices by backward elimination

--- SuperDataScience/regression/multivariate_regression.py
+++ SuperDataScience/regression/multivariate_regression.py
@@ -59,3 +59,82 @@ regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 y_pred = regressor.predict(X_test)
+
+# Choosing right features - building an optimal model by using backward elimination
+
+import statsmodels.formula.api as sm
+# add column of 1 here representing the free coefficient in regression equation
+
+X = np. append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+
+# backward elimination
+# X_opt - includes only all the variables that are statistically significant
+X_opt = X[:, [0,1,2,3,4,5]]
+# regressor_OLS ordinary least squares
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+#final set of features chosen - actually - one variable and the free parameter
+X_opt = X[:, [0,3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+# ADDITIONAL STUFF
+# automated backward elimination with parameters - initial X set with all features and significancy level
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
