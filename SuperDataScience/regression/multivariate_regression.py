4e345b08fe89b0a236f8a2c3f246a6b7167532d4
LOpuchlik <lidia.opuchlik@gmail.com>
2020-02-24 20:29:46 +0100

    Added file with multiple linear regression code

---
+++ SuperDataScience/regression/multivariate_regression.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 20:25:10 2020
+
+@author: jagoodka
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+dataset = pd.read_csv('50_Startups.csv')
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
