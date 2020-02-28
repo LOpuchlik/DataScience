f2faf5a6725a05b18358900ac462ba28722a24ce
Michal <michal.opuchlik@gmail.com>
2020-02-28 18:46:03 +0100

    Added volume_slider and granted turn up/down functionality

--- Music_player/TKinter_MP/main.py
+++ Music_player/TKinter_MP/main.py
@@ -1,6 +1,10 @@
 from tkinter import *
+from pygame import mixer
 
 root = Tk()
+
+mixer.init()
+
 root.geometry('550x300')
 root.title("Jagoda's Music Player")
 root.iconbitmap(r'main_icon.ico')
@@ -10,31 +14,49 @@ text = Label(root, text = "Let's listen to some music!!!").pack()
 # opacity/tranparency applies to image and frame
 root.wm_attributes('-alpha', 0.8)
 
+def play():
+    mixer.music.load('sample.wav')
+    mixer.music.play()
+
+def stop():
+    mixer.music.stop()
+
+def adjust_volume(val):
+    volume = int(val)/100
+    mixer.music.set_volume(volume)
+
+
 
 play_pic = PhotoImage(file ='play-button.png')
 play_pic = play_pic.zoom(5) #with 250, I ended up running out of memory
 play_pic = play_pic.subsample(55) #mechanically, here it is adjusted to 32 instead of 320
-# play_label = Label(root, image = play_pic).pack()
-
-def play():
-    print("Music plays!!")
 play_button = Button(root, image = play_pic, command = play).pack(side = LEFT)
-
-
-pause_pic = PhotoImage(file ='pause.png')
-pause_pic = pause_pic.zoom(5) #with 250, I ended up running out of memory
-pause_pic = pause_pic.subsample(55) #mechanically, here it is adjusted to 32 instead of 320
-
-def pause():
-    print('Music paused!')
-pause_button = Button(root, image = pause_pic, command = pause).pack(side = LEFT)
 
 stop_pic = PhotoImage(file ='stop.png')
 stop_pic = stop_pic.zoom(5) #with 250, I ended up running out of memory
 stop_pic = stop_pic.subsample(55) #mechanically, here it is adjusted to 32 instead of 320
-
-def stop():
-    print('Music stopped!')
 stop_button = Button(root, image = stop_pic, command = stop).pack(side = LEFT)
+
+volume_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, command = adjust_volume).pack(side = LEFT)
+
+
+
+
+
+
+"""
+def pause():
+    mixer.music.pause()
+
+pause_pic = PhotoImage(file ='pause.png')
+pause_pic = pause_pic.zoom(5) #with 250, I ended up running out of memory
+pause_pic = pause_pic.subsample(55) #mechanically, here it is adjusted to 32 instead of 320
+pause_button = Button(root, image = pause_pic, command = pause).pack(side = LEFT)
+
+"""
+
+
+
+
 
 root.mainloop()
