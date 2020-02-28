02cdd5effa01f5f78cd71aae6d5b420c1e5b958d
LOpuchlik <lidia.opuchlik@gmail.com>
2020-02-28 20:00:21 +0100

    Cleaned the mess

---
+++ SuperDataScience/classification/classification_template.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 11:45:51 2020
+
+@author: jagoodka
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 20:38:36 2020
+
+@author: jagoodka
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('.csv')
+
+X = dataset.iloc[:, 2:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+scaler_X = StandardScaler()
+X_train = scaler_X.fit_transform(X_train)
+X_test = scaler_X.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+
+# fitting
+# create a classifier
+
+# prediction
+y_pred = classifier.predict(X_test)
+
+
+from sklearn.metrics import confusion_matrix
+
+# Confusion matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+print(conf_matrix)
+
+
+# Visualisations
+# train set results
+conf_matr_visual(X_train, y_train)
+
+# test set results
+conf_matr_visual(X_test, y_test)
+
+
+def conf_matr_visual(X, y):
+    from matplotlib.colors import ListedColormap
+    X_set, y_set = X, y
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.5, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title('')
+    plt.xlabel('')
+    plt.ylabel('')
+    plt.legend()
+    plt.show()
+
+
+
+
+
+
+
+
